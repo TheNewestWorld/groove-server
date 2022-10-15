6adee2a7197ea6e2a316bd1205e80ccc8860cb8a
1n9yun <jig7357@gmail.com>
2022-10-16 00:22:08 +0900

    feat: swagger Jwt로 인증 가능하도록 수정

--- groove-api/src/main/java/org/bogus/groove/config/SwaggerConfig.java
+++ groove-api/src/main/java/org/bogus/groove/config/SwaggerConfig.java
@@ -1,15 +1,28 @@
 package org.bogus.groove.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-    name = "Bearer Authentication",
-    type = SecuritySchemeType.HTTP,
-    bearerFormat = "JWT",
-    scheme = "bearer"
-)
-public class SwaggerConfig { }
-
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement().addList("Authorization"))
+            .components(
+                new Components()
+                    .addSecuritySchemes(
+                        "Authorization",
+                        new SecurityScheme()
+                            .name("Authorization")
+                            .type(SecurityScheme.Type.APIKEY)
+                            .in(SecurityScheme.In.HEADER)
+                            .bearerFormat("JWT")
+                    )
+            );
+    }
+}
