ef49fd359e8c63f3462d1cb12a45e85ea7f9a432
1n9yun <jig7357@gmail.com>
2022-10-23 17:07:29 +0900

    oauth 관련 클래스 봉인

--- groove-security/spring-security/src/main/java/org/bogus/groove/config/oauth/GoogleUserInfo.java
+++ groove-security/spring-security/src/main/java/org/bogus/groove/config/oauth/GoogleUserInfo.java
@@ -1,37 +1,37 @@
-package org.bogus.groove.config.oauth;
-
-import org.bogus.groove.domain.user.UserType;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-
-public class GoogleUserInfo implements OAuth2UserInfo {
-    private final OAuth2AuthenticationToken authentication;
-
-    public GoogleUserInfo(OAuth2AuthenticationToken authentication) {
-        this.authentication = authentication;
-    }
-
-    @Override
-    public UserType getUserType() {
-        return UserType.GOOGLE;
-    }
-
-    @Override
-    public String getEmail() {
-        return authentication.getPrincipal().getAttribute("email");
-    }
-
-    @Override
-    public String getName() {
-        return authentication.getPrincipal().getAttribute("name");
-    }
-
-    @Override
-    public String getProfileImage() {
-        return authentication.getPrincipal().getAttribute("picture");
-    }
-
-    @Override
-    public String getUserId() {
-        return authentication.getPrincipal().getName(); // ID?
-    }
-}
+//package org.bogus.groove.config.oauth;
+//
+//import org.bogus.groove.domain.user.UserType;
+//import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+//
+//public class GoogleUserInfo implements OAuth2UserInfo {
+//    private final OAuth2AuthenticationToken authentication;
+//
+//    public GoogleUserInfo(OAuth2AuthenticationToken authentication) {
+//        this.authentication = authentication;
+//    }
+//
+//    @Override
+//    public UserType getUserType() {
+//        return UserType.GOOGLE;
+//    }
+//
+//    @Override
+//    public String getEmail() {
+//        return authentication.getPrincipal().getAttribute("email");
+//    }
+//
+//    @Override
+//    public String getName() {
+//        return authentication.getPrincipal().getAttribute("name");
+//    }
+//
+//    @Override
+//    public String getProfileImage() {
+//        return authentication.getPrincipal().getAttribute("picture");
+//    }
+//
+//    @Override
+//    public String getUserId() {
+//        return authentication.getPrincipal().getName(); // ID?
+//    }
+//}
