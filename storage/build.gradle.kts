5c90a0416c96b6280917268153de6d2001c8dee1
nahyunii <=>
2022-10-29 22:37:26 +0900

    querydsl setting 추가

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern.compile
+
 val springBootVersion: String by project
 val h2Version: String by project
 val postgresqlVersion: String by project
@@ -9,9 +11,18 @@ dependencies {
 
     implementation("com.h2database:h2:$h2Version")
     implementation("org.postgresql:postgresql:$postgresqlVersion")
-    implementation ("com.querydsl:querydsl-jpa:$queryDslVersion")
-    annotationProcessor (
-            "javax.persistence:javax.persistence-api",
-            "javax.annotation:javax.annotation-api",
-            "com.querydsl:querydsl-apt:$queryDslVersion:jpa")
+    implementation("com.querydsl:querydsl-jpa:$queryDslVersion")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api:2.2.3")
+    annotationProcessor("com.querydsl:querydsl-apt:$queryDslVersion:jpa")
 }
+
+// QueryDSL
+sourceSets {
+    main {
+        java {
+            srcDirs ("$projectDir/src/main/java")
+            srcDirs ("$projectDir/build/generated")
+        }
+    }
+}
+
