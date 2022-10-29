2170b51d86cf05758da0ad0d6cc0a715bdc5a6b0
nahyunii <=>
2022-10-29 22:35:44 +0900

    redis maxmemory config 추가

--- redis/src/main/java/org/bogus/groove/redis/config/TestRedisConfig.java
+++ redis/src/main/java/org/bogus/groove/redis/config/TestRedisConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
+import redis.embedded.RedisServerBuilder;
 
 @Profile({"test", "local"})
 @Configuration
@@ -15,7 +16,10 @@ public class TestRedisConfig {
     private final RedisServer redisServer;
 
     public TestRedisConfig(RedisProperties redisProperties) {
-        this.redisServer = new RedisServer(redisProperties.getPort());
+        this.redisServer = RedisServer.builder()
+            .port(redisProperties.getPort())
+            .setting("maxmemory 128M")
+            .build();
     }
 
     @PostConstruct
