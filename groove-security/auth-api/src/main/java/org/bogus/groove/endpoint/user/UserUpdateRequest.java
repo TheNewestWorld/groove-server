d1f587133ab68a333c13d3a0bb0617eea3b05e4a
1n9yun <jig7357@gmail.com>
2022-11-04 20:06:56 +0900

    feat: 유저 닉네임, 프로푈 업데이트 API 추가

---
+++ groove-security/auth-api/src/main/java/org/bogus/groove/endpoint/user/UserUpdateRequest.java
@@ -0,0 +1,12 @@
+package org.bogus.groove.endpoint.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserUpdateRequest {
+    private String nickname;
+}
