32f7cc842bfb9014ca921d61490453608394fe2e
1n9yun <jig7357@gmail.com>
2022-11-05 22:15:08 +0900

    refactor: attachment 관련 컴포넌트 위치 변경

--- groove-api/src/main/java/org/bogus/groove/domain/attachment/AttachmentService.java
+++ groove-api/src/main/java/org/bogus/groove/domain/attachment/AttachmentService.java
@@ -7,19 +7,16 @@ import org.bogus.groove.common.exception.ForbiddenException;
 import org.bogus.groove.object_storage.AttachmentAuthorityChecker;
 import org.bogus.groove.object_storage.AttachmentDownload;
 import org.bogus.groove.object_storage.AttachmentDownloader;
-import org.bogus.groove.object_storage.AttachmentReader;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AttachmentService {
     private final AttachmentDownloader downloader;
-    private final AttachmentReader attachmentReader;
     private final AttachmentAuthorityChecker attachmentAuthorityChecker;
 
     public AttachmentDownload download(String objectKey, AttachmentType attachmentType, Long userId) {
-        var attachment = attachmentReader.read(objectKey, attachmentType);
-        var authorized = attachmentAuthorityChecker.check(attachment.getId(), AttachmentType.PRIVATE_RECORD, userId);
+        var authorized = attachmentAuthorityChecker.check(objectKey, AttachmentType.PRIVATE_RECORD, userId);
         if (!authorized) {
             throw new ForbiddenException(ErrorType.FORBIDDEN_NOT_ENOUGH_AUTHORITY);
         }
