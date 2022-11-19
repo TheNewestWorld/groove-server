d6e4425129b1bf0b9330dee1079a9aa927f7984f
1n9yun <jig7357@gmail.com>
2022-11-19 14:47:53 +0900

    AppException 추상 클래스로 변경

--- common/src/main/java/org/bogus/groove/common/exception/AppException.java
+++ common/src/main/java/org/bogus/groove/common/exception/AppException.java
@@ -1,20 +1,17 @@
 package org.bogus.groove.common.exception;
 
 import java.util.Map;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
-public class AppException extends RuntimeException {
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public abstract class AppException extends RuntimeException {
     @Getter
     private final ErrorType error;
     private final String extraMessage;
     @Getter
     private final Map<String, Object> extraData;
-
-    protected AppException(ErrorType error, String extraMessage, Map<String, Object> extraData) {
-        this.error = error;
-        this.extraMessage = extraMessage;
-        this.extraData = extraData;
-    }
 
     public String getMessage() {
         if (extraMessage == null) {
