8a11fa4c570e4381ae9e905437223afe4bceea7f
HEEJIN <heehee2310@gmail.com>
2022-11-27 01:58:41 +0900

    feat: email template 적용 초안

--- mail/src/main/java/org/bogus/groove/mail/config/GoogleMailSender.java
+++ mail/src/main/java/org/bogus/groove/mail/config/GoogleMailSender.java
@@ -1,5 +1,8 @@
 package org.bogus.groove.mail.config;
 
+import java.util.HashMap;
+import java.util.Map;
+import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bogus.groove.common.exception.ErrorType;
 import org.bogus.groove.common.exception.InternalServerException;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,12 +19,13 @@ import org.springframework.stereotype.Service;
 public class GoogleMailSender {
 
     private final JavaMailSender sender;
+    private final SpringTemplateEn
 
     // TODO : 프론트 상의 후 링크 변경 필요
     private static final String CHANGE_PASSWORD_LINK = "http://localhost:8080";
     private static final String EMAIL_AUTHENTICATION_LINK = "http://localhost:8080";
 
-    private MimeMessage getPasswordChangeMessage(String to, String sessionKey) {
+    private MimeMessage getPasswordChangeMessage(String to, String sessionKey) throws MessagingException {
         MimeMessage message = sender.createMimeMessage();
 
         try {
@@ -28,7 +33,9 @@ public class GoogleMailSender {
             message.setSubject("[Groove] 비밀번호 변경 요청");
 
             StringBuilder sb = new StringBuilder();
+            sb.append("<h1>d안영 </ㅗ1>");
             sb.append(CHANGE_PASSWORD_LINK).append("/").append(sessionKey);
+
             message.setText(sb.toString(), "utf-8");
             message.setFrom(new InternetAddress("noreply@groove.com", "groove"));
         } catch (Exception e) {
@@ -42,8 +49,18 @@ public class GoogleMailSender {
         MimeMessage message = sender.createMimeMessage();
 
         try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setSubject("[Groove] 이메일 인증 요청");
+            helper.setTo(to);
+
+            Map<String, String> emailValues = new HashMap<>();
+//            emailValues.put("")
+
+            String html = templateEng
+
             message.addRecipients(MimeMessage.RecipientType.TO, to);
-            message.setSubject("[Groove] 이메일 인증 요청");
+            message.setSubject("");
 
             StringBuilder sb = new StringBuilder();
             sb.append(EMAIL_AUTHENTICATION_LINK).append("/").append(sessionKey);
