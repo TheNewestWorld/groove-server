a9970abc7619b3a6ce2c7ebec0151bb89e1d4159
1n9yun <jig7357@gmail.com>
2022-12-06 03:14:59 +0900

    feat: 기본 프로필 저장

--- groove-security/auth/src/main/java/org/bogus/groove/domain/user/UserRegister.java
+++ groove-security/auth/src/main/java/org/bogus/groove/domain/user/UserRegister.java
@@ -2,8 +2,11 @@ package org.bogus.groove.domain.user;
 
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.bogus.groove.common.enumeration.AttachmentType;
 import org.bogus.groove.common.exception.BadRequestException;
 import org.bogus.groove.common.exception.ErrorType;
+import org.bogus.groove.object_storage.AttachmentCreateParam;
+import org.bogus.groove.object_storage.AttachmentCreator;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,13 +14,28 @@ import org.springframework.stereotype.Component;
 public class UserRegister {
     private final UserCreator userCreator;
     private final UserReader userReader;
+    private final AttachmentCreator attachmentCreator;
 
     @Transactional
     public User register(UserRegisterParam param) {
         validateNotDuplicated(param);
 
-        var created = userCreator.create(param);
-        return userReader.read(created.getId());
+        var user = userCreator.create(param);
+        createDefaultProfile(user.getId());
+        return user;
+    }
+
+    private void createDefaultProfile(Long userId) {
+        attachmentCreator.create(
+            new AttachmentCreateParam(
+                "default",
+                null,
+                "default",
+                0,
+                userId,
+                AttachmentType.PROFILE
+            )
+        );
     }
 
     private void validateNotDuplicated(UserRegisterParam param) {
