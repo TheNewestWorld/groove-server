36311b2d18001aef4a38ca2ee2d3d7cf3742cd45
yonguk929@gmail.com <yonguk929@gmail.com>
2022-12-12 23:42:14 +0900

    delete : 불필요한 파일 삭제

--- storage/src/main/java/org/bogus/groove/storage/repository/ReportRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package org.bogus.groove.storage.repository;
-
-import org.bogus.groove.storage.entity.ReportEntity;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-
-public interface ReportRepositoryCustom {
-    Slice<ReportEntity> reportAllPosts(Pageable pageable);
-}
