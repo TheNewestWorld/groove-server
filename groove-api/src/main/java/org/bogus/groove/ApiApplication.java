2411185db2658b72e022853a7c4bd6c561bb9574
1n9yun <jig7357@gmail.com>
2022-12-20 00:52:04 +0900

    fix: swagger 상대주소로 요청하도록 수정

--- groove-api/src/main/java/org/bogus/groove/ApiApplication.java
+++ groove-api/src/main/java/org/bogus/groove/ApiApplication.java
@@ -1,9 +1,12 @@
 package org.bogus.groove;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class ApiApplication {
