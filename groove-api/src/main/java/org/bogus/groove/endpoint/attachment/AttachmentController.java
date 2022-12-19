ccf43354ce6ad9584b966acf1c30cfa14c9052c7
1n9yun <jig7357@gmail.com>
2022-12-20 01:47:30 +0900

    fix: 파일 요청 시 인증정보가 필요한 경우에만 검증하도록 변경

--- groove-api/src/main/java/org/bogus/groove/endpoint/attachment/AttachmentController.java
+++ groove-api/src/main/java/org/bogus/groove/endpoint/attachment/AttachmentController.java
@@ -3,17 +3,17 @@ package org.bogus.groove.endpoint.attachment;
 import io.swagger.v3.oas.annotations.Hidden;
 import java.nio.charset.StandardCharsets;
 import lombok.RequiredArgsConstructor;
+import org.bogus.groove.client.user.UserClient;
+import org.bogus.groove.client.user.UserInfo;
 import org.bogus.groove.common.enumeration.AttachmentType;
-import org.bogus.groove.config.GrooveUserDetails;
 import org.bogus.groove.domain.attachment.AttachmentService;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
@@ -21,16 +21,16 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 @RequiredArgsConstructor
 public class AttachmentController {
     private final AttachmentService attachmentService;
+    private final UserClient userClient;
 
     @Hidden
-    @Secured("permitAll")
     @GetMapping("/attachments/{attachmentType}/{objectKey}")
     public ResponseEntity<StreamingResponseBody> download(
+        @RequestHeader(value = HttpHeaders.AUTHORIZATION, required = false) String accessToken,
         @PathVariable AttachmentType attachmentType,
-        @PathVariable String objectKey,
-        @AuthenticationPrincipal GrooveUserDetails userDetails
+        @PathVariable String objectKey
     ) {
-        var userId = userDetails == null ? null : userDetails.getUserId();
+        var userId = userClient.getOrNull(accessToken).map(UserInfo::getId).orElse(null);
         var download = attachmentService.download(objectKey, attachmentType, userId);
 
         return ResponseEntity.ok()
--- groove-security/auth-api/src/main/java/org/bogus/groove/endpoint/user/InternalUserController.java
+++ groove-security/auth-api/src/main/java/org/bogus/groove/endpoint/user/InternalUserController.java
@@ -1,16 +1,28 @@
 package org.bogus.groove.endpoint.user;
 
 import lombok.RequiredArgsConstructor;
-import org.bogus.groove.domain.user.UserService;
+import org.bogus.groove.domain.user.UserInfoFinder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 public class InternalUserController {
-    private final UserService userService;
+    private final UserInfoFinder userInfoFinder;
 
     public InternalUserInfoGetResponse getUserInfo(Long userId) {
-        var result = userService.getUserInfo(userId);
+        var result = userInfoFinder.find(userId);
+        return new InternalUserInfoGetResponse(
+            result.getId(),
+            result.getEmail(),
+            result.getProviderType(),
+            result.getNickName(),
+            result.getProfileUri(),
+            result.getRole()
+        );
+    }
+
+    public InternalUserInfoGetResponse getUserInfo(String token) {
+        var result = userInfoFinder.find(token);
         return new InternalUserInfoGetResponse(
             result.getId(),
             result.getEmail(),
