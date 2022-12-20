71a015791e1faee927f3b7e118d834a1e5b8fc0e
1n9yun <jig7357@gmail.com>
2022-12-21 01:09:45 +0900

    개인 녹음 파일도 public 으로 임시 변경

--- common/src/main/java/org/bogus/groove/common/enumeration/AttachmentType.java
+++ common/src/main/java/org/bogus/groove/common/enumeration/AttachmentType.java
@@ -7,7 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AttachmentType {
     PROFILE("profile", true),
-    PRIVATE_RECORD("private-record", false),
+    // TODO 프론트에서 파일 다운로드 시 헤더에 토큰 넣는게 힘든가봄 가능할 것 같은뒤,, 일단 패스하기로 함 여유될 때 확인
+    PRIVATE_RECORD("private-record", true),
     POST_IMAGE("post-image", true),
     POST_RECORD("post-record", true)
     ;
