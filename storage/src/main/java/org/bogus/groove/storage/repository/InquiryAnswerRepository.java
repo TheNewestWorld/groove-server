00e57b9b34a84b67c87046f9199cefea2101abb0
yonguk929@gmail.com <yonguk929@gmail.com>
2022-12-22 18:59:26 +0900

    rebase : 문의하기 기능 삭제

--- storage/src/main/java/org/bogus/groove/storage/repository/InquiryAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package org.bogus.groove.storage.repository;
-
-import org.bogus.groove.storage.entity.InquiryAnswerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InquiryAnswerRepository extends JpaRepository<InquiryAnswerEntity, Long> {
-}
